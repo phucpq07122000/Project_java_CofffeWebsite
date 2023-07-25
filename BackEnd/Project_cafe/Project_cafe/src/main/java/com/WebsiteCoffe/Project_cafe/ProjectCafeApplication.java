53ad29df0c727ff19d167b076c521ccffc068a9a
phucpq07122000 <122593397+phucpq07122000@users.noreply.github.com>
2023-07-25 15:39:16 +0700

    BE_upload

---
+++ BackEnd/Project_cafe/Project_cafe/src/main/java/com/WebsiteCoffe/Project_cafe/ProjectCafeApplication.java
@@ -0,0 +1,13 @@
+package com.WebsiteCoffe.Project_cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCafeApplication.class, args);
+	}
+
+}
---
+++ BackEnd/Project_cafe/Project_cafe/src/test/java/com/WebsiteCoffe/Project_cafe/ProjectCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WebsiteCoffe.Project_cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
